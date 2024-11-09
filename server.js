9e50761b4d29a4ecccf12d5bbd07b0fd93d75eba
Hardik Marlapudi <hardik.a.marlapudi@gmail.com>
2024-11-09 10:59:06 -0500

    adding cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+app.use(cors());
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Render will set process.env.PORT
